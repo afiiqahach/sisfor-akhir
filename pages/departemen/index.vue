e519efd0ce312121eb40f82b771a889b81fbb33f
afiqahdwi1@gmail.com <afiqahdwi1@gmail.com>
2023-12-09 15:16:22 +0800

    first commit

---
+++ pages/departemen/index.vue
@@ -0,0 +1,118 @@
+<template>
+    <main class="overflow-x-hidden">
+        <!-- header -->
+        <section class="pt-24">
+            <div class="h-[202px] relative">
+                <img class="w-[100%] left-0 top-2 absolute" src="../assets/image/jkcenterhlm.png" />
+                <div class="w-[474px] h-[45px] relative top-20 left-20">
+                    <a href="#" class="w-[302px] h-[44px] left-[172px] absolute text-slate-300 text-2xl font-medium">Departemen</a>
+                    <a href="#" class="w-[180px] h-[44px] left-0 absolute text-center text-sky-950 text-2xl font-medium">Halaman</a>
+                    <a href="#" class="w-[180px] h-[44px] left-16 pt-1 absolute text-center text-sky-950 text-xl font-medium">  > </a>
+                </div>
+            </div>
+        </section>
+        <!-- header end -->
+
+        <section class="pt-8">
+            <div class="ml-28 text-black text-3xl font-bold">Departemen di Fakultas Teknik Universitas Hasanuddin
+                <img src="../assets/image/Rectangle19.png" class="ml-100">
+            </div>
+            <div class="ml-28 mt-5 mb-20 pr-56 text-base">
+                <div>
+                    <h5 class="text-sky-700">1. Departemen Teknik Sipil</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Sipil (Akreditasi Unggul BAN-PT, Akreditasi Internasional IABEE, Akreditasi Internasional ABET, Sertifikat AUN-QA)</li>
+                        <li>Program Magister Teknik Sipil (Akreditasi A BAN-PT)</li>
+                        <li>Program Doktor Teknik Mesin (Akreditasi B BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">2. Departemen Teknik Mesin</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Mesin (Akreditasi Unggul BAN-PT, Akreditasi Internasional ASIIN)</li>
+                        <li>Program Magister Teknik Mesin (Akreditasi Unggul LAMTEK)</li>
+                        <li>Program Doktor Ilmu Teknik Sipil (Akreditasi Unggul BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">3. Departemen Teknik Perkapalan</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Perkapalan (Akreditasi Unggul LAMTEK, Akreditasi Internasional ASIIN)</li>
+                        <li>Program Magister Teknik Perkapalan (Akreditasi B BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">4. Departemen Teknik Elektro</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Elektro (Akreditasi Unggul LAMTEK, Akreditasi Internasional IABEE, Akreditasi Internasional ABET)</li>
+                        <li>Program Magister Teknik Elektro (Akreditasi Unggul LAMTEK)</li>
+                        <li>Program Doktor Teknik Elektro (Akreditasi B BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">5. Departemen Teknik Arsitektur</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Arsitektur (Akreditasi Unggul BAN-PT, Sertifikat AUN-QA)</li>
+                        <li>Program Magister Arsitektur (Akreditasi B BAN-PT)</li>
+                        <li>Program Doktor Ilmu Arsitektur (Akreditasi A BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">6. Departemen Teknik Geologi</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Geologi (Akreditasi Unggul BAN-PT, Sertifikat AUN-QA)</li>
+                        <li>Program Magister Geologi (Akreditasi B BAN-PT)</li>
+                        <li>Program Doktor Teknologi Kebumian dan Lingkungan (Akreditasi B BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">7. Departemen Teknik Industri</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Industri (Akreditasi A BAN-PT, Akreditasi Internasional ASIIN)</li>
+                        <li>Program Magister Teknik Industri (Akreditasi Baik BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">8. Departemen Teknik kelautan</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Kelautan (Akreditasi Unggul BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">9. Departemen Teknik Sistem Perkapalan</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Sistem Perkapalan (Akreditasi Unggul BAN-PT, Akreditasi Internasional ASIIN)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">10. Departemen Teknik Perencanaan Wilayah Kota</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Perencanaan Wilayah dan Kota (Akreditasi A BAN-PT, Akreditasi Internasional ASIIN)</li>
+                        <li>Program Magister Perencanaan Wilayah dan Kota (Akreditasi Baik BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">11. Departemen Teknik Pertambangan</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Pertambangan (Akreditasi A BAN-PT, Akreditasi Internasional ASIIN)</li>
+                        <li>Program Magister Teknik Pertambangan (Akreditasi Baik BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">12. Departemen Teknik Informatika</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Informatika (Akreditasi Unggul BAN-PT:, Akreditasi Internasional: ASIIN)</li>
+                        <li>Program Magister Teknik Informatika (Akreditasi Baik Sekali BAN-PT)</li>
+                    </ul>
+                </div> 
+                <div>
+                    <h5 class="text-sky-700">13. Departemen Teknik Lingkungan</h5>
+                    <ul class="list-outside list-disc ml-8 mb-4">
+                        <li>Program Sarjana Teknik Lingkungan (Akreditasi Unggul BAN-PT, Akreditasi Internasional IABEE)</li>
+                        <li>Program Magister Teknik Lingkungan (Akreditasi Baik Sekali BAN-PT)</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
---
+++ pages/strukturorganisasi/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <main class="overflow-x-hidden">
+        <!-- header -->
+        <section class="pt-24">
+            <div class="h-[202px] relative">
+                <img class="w-[100%] left-0 top-2 absolute" src="../assets/image/jkcenterhlm.png" />
+                <div class="w-[474px] h-[45px] relative top-20 left-20">
+                    <a href="#" class="w-[302px] h-[44px] left-[172px] absolute text-slate-300 text-2xl font-medium">Struktur Organisasi</a>
+                    <a href="#" class="w-[180px] h-[44px] left-0 absolute text-center text-sky-950 text-2xl font-medium">Halaman</a>
+                    <a href="#" class="w-[180px] h-[44px] left-16 pt-1 absolute text-center text-sky-950 text-xl font-medium">  > </a>
+                </div>
+            </div>
+        </section>
+        <!-- header end -->
+
+        <section class="pt-8">
+            <div class="ml-28 text-black text-3xl font-bold">STRUKTUR ORGANISASI
+                <img src="../assets/image/Rectangle19.png" class="ml-100">
+            </div>
+            <img src="../assets/image/struktur.png" alt="struktur" class="pt-16 pb-20 px-80">
+        </section>
+    </main>
+</template>
